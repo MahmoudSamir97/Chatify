3b08f29275a5178def61f43dd5c5ff73c954b9cb
Mahmoud Samir <mahmodsamir28@gmail.com>
2024-04-22 17:07:27 +0200

    fix scrolling to last message

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,21 +1,35 @@
 import React from "react";
+import { useAuthContext } from "../../context/AuthContext";
+import useConversation from "../../zustand/useConversation";
+import avatarImg from "../../assets/images/avatar.png";
+import { extractTime } from "../../utils/extractTime";
 
-function Message() {
+function Message({ message }) {
+  const { authUser } = useAuthContext();
+  const { selectedConversation } = useConversation();
+  const fromMe = message.senderId === authUser._id;
+  console.log(fromMe);
+  const formattedTime = extractTime(message.createdAt);
+  const chatClassName = fromMe ? "chat-end" : "chat-start";
+  const profilePic = fromMe
+    ? authUser.profilePic
+    : selectedConversation?.profilePic;
+  const bubbleBgColor = fromMe ? "bg-blue-500" : "";
   return (
-    <div className={`chat chat-end`}>
+    <div className={`chat ${chatClassName}`}>
       <div className="chat-image avatar">
         <div className="w-10 rounded-full">
           <img
             alt="Tailwind CSS chat bubble component"
-            src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+            src={profilePic || avatarImg}
           />
         </div>
       </div>
-      <div className={`chat-bubble text-white bg-blue-500  pb-2`}>
-        Hi whats app? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      <div className={`chat-bubble text-white pb-2 ${bubbleBgColor}`}>
+        {message.message}
       </div>
       <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">
-        12:24
+        {formattedTime}
       </div>
     </div>
   );
--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Message from "./Message";
+import useGetMessages from "../../hooks/useGetMessages";
+import MessageSkeleton from "../skeletons/MessageSceleton";
 
 function Messages() {
+  const { loading, messages } = useGetMessages();
+  const lastMsgRef = useRef();
+
+  useEffect(() => {
+    setTimeout(() => {
+      lastMsgRef.current?.scrollIntoView({ behavior: "smooth" });
+    }, 100);
+  }, [messages]);
   return (
     <div
       className="px-4 flex-1 overflow-auto"
@@ -9,22 +19,19 @@ function Messages() {
         overflowY: "scroll",
       }}
     >
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
+      {!loading &&
+        messages.length > 0 &&
+        messages.map((message) => (
+          <div key={message._id} ref={lastMsgRef}>
+            <Message message={message} />
+          </div>
+        ))}
+      {loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
+      {!loading && messages.length === 0 && (
+        <p className="text-center text text-sm text-gray-400">
+          Send a message to start the conversation
+        </p>
+      )}
     </div>
   );
 }
