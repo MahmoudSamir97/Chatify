1051a8b8d33c1dca5a1f5b558b93ea6011a63727
Mahmoud Samir <mahmodsamir28@gmail.com>
2024-04-22 17:05:56 +0200

    handling conversations component

--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -1,26 +1,35 @@
 import React from "react";
+import useConversation from "../../zustand/useConversation";
 
-function Conversation() {
+import imgPlaceHolder from "./../../assets/images/avatar.png";
+
+function Conversation({ conversation, lastIdx }) {
+  const { selectedConversation, setSelectedConversation } = useConversation();
+  const isSelected = selectedConversation?._id === conversation._id;
   return (
     <>
-      <div className="flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer">
+      <div
+        className={`flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer ${
+          isSelected ? "bg-sky-500" : ""
+        }`}
+        onClick={() => setSelectedConversation(conversation)}
+      >
         <div className="avatar online">
           <div className="w-12 rounded-full">
             <img
-              src="https://cdn0.iconfinder.com/data/icons/communication-line-10/24/account_profile_user_contact_person_avatar_placeholder-512.png"
-              alt="user avatar"
+              src={conversation.profilePic || imgPlaceHolder}
+              alt="user profile"
             />
           </div>
         </div>
 
         <div className="flex flex-col flex-1">
           <div className="flex gap-3 justify-between">
-            <p className="font-bold text-gray-200">John Doe</p>
+            <p className="font-bold text-gray-200">{conversation.username}</p>
           </div>
         </div>
       </div>
-
-      <div className="divider my-0 py-0 h-1" />
+      {!lastIdx && <div className="divider my-0 py-0 h-1" />}
     </>
   );
 }
--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import Conversation from "./Conversation";
+import useGetConversation from "../../hooks/useGetConversation";
 
 function Conversations() {
+  const { loading, conversations } = useGetConversation();
   return (
     <div className="py-2 flex flex-col overflow-auto">
-      <Conversation />
-      <Conversation />
-      <Conversation />
-      <Conversation />
-      <Conversation />
-      <Conversation />
+      {conversations.map((conv, idx) => (
+        <Conversation
+          key={conv._id}
+          conversation={conv}
+          lastIdx={idx === conversations.length - 1}
+        />
+      ))}
+      {loading ? (
+        <span className="loading loading-spinner mx-auto"></span>
+      ) : null}
     </div>
   );
 }
