5cc989a2ae89b937f46f91f1c94a1df8cb4c0168
Mahmoud Samir <mahmodsamir28@gmail.com>
2024-04-22 16:50:24 +0200

    implement search for users feature

--- frontend/src/components/sidebar/SearchInput.jsx
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { IoSearchSharp } from "react-icons/io5";
+import useConversation from "../../zustand/useConversation";
+import useGetConversation from "../../hooks/useGetConversation";
+import toast from "react-hot-toast";
 
 function SearchInput() {
+  const [search, setSearch] = useState("");
+  const { setSelectedConversation } = useConversation();
+  const { conversations } = useGetConversation();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!search) return;
+    if (search.length < 3)
+      return toast.error("Search input must be at least 3 characters");
+    const conversation = conversations.find((c) =>
+      c.username.toLowerCase().includes(search.toLowerCase())
+    );
+    if (conversation) {
+      setSelectedConversation(conversation);
+    } else {
+      toast.error("No such user found");
+    }
+  };
   return (
-    <form className="flex items-center gap-2">
+    <form onSubmit={handleSubmit} className="flex items-center gap-2">
       <input
         type="text"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
         placeholder="Search…"
         className="input input-bordered rounded-full text-white bg-gray-900"
       />
